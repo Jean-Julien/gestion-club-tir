7d1d07adbd50ec188a954c3495383c0b0fa9aede
R3V3NG3 <petitpiqueur@gmail.com>
2021-11-02 22:12:37 +0100

    Système de login avec DB

--- view/pages/login.php
+++ view/pages/login.php
@@ -24,12 +24,12 @@
 
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                        <input type="email" class="form-control" id="exampleInputEmail1" name="emailLogin" aria-describedby="emailHelp" placeholder="Email">
                     </div>
 
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" class="form-control" id="exampleInputPassword1" name="passwordLogin" placeholder="Password">
                     </div>
                     
                     <label class="mb-3">
