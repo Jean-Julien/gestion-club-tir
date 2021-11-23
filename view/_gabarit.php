87d4228e8aab3d2d4e14161df0fdefc30c7e3890
R3V3NG3 <petitpiqueur@gmail.com>
2021-11-23 12:33:30 +0100

    Test

--- view/_gabarit.php
+++ view/_gabarit.php
@@ -3,7 +3,6 @@
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 </head>
 
 <body>
@@ -37,7 +36,7 @@
         ?>
 
     </div>
-    <div class="footer">
+    <div class="footer fixed-bottom">
         <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
             © 2021 Copyright:
             <a class="text-reset fw-bold">TKT</a>
--- view/pages/home.php
+++ view/pages/home.php
@@ -22,49 +22,73 @@
 </div>
 
 <div class="container">
-    
     <form role="form" action="myaction.php" method="post">
+        <div class="mt-2">
+            <input type="text" class="form-control w-25" id="pseudo" placeholder="Pseudo">
+        </div>
 
         <div class="mt-2">
-        <input type="date" class="form-control w-25" id="reservation">
-        <select class="form-control w-25 mt-2" id="horaire">
-            <option value="horaire1">08:00 - 08:30</option>
-            <option value="horaire2" disabled>09:00 - 09:30</option>
-            <option value="horaire3" >10:00 - 10:30</option>
-            <option value="horaire4" >11:00 - 11:30</option>
-            <option value="horaire5" >12:00 - 12:30</option>
-            <option value="horaire6" >13:00 - 13:30</option>
-            <option value="horaire7" >14:00 - 14:30</option>
-            <option value="horaire8" >15:00 - 15:30</option>
-            <option value="horaire9" >16:00 - 16:30</option>
-        </select> 
+            <input type="date" class="form-control w-25" id="reservation">
         </div>
+
         <div class="mt-2">
-        <select class="form-control w-25" id="pastir">
-            <option value="Pas de tir 1" disabled>Pas de tir 1</option>
-            <option value="Pas de tir 2">Pas de tir 2</option>
-            <option value="Pas de tir 3">Pas de tir 3</option>
-            <option value="Pas de tir 4">Pas de tir 4</option>
-            <option value="Pas de tir 5">Pas de tir 5</option>
-            <option value="Pas de tir 6">Pas de tir 6</option>
-            <option value="Pas de tir 7">Pas de tir 7</option>
-            <option value="Pas de tir 8">Pas de tir 8</option>
-            <option value="Pas de tir 9">Pas de tir 9</option>
-            <option value="Pas de tir 10">Pas de tir 10</option>
-            <option value="Pas de tir 11">Pas de tir 11</option>
-            <option value="Pas de tir 12">Pas de tir 12</option>
-            <option value="Pas de tir 13">Pas de tir 13</option>
-            <option value="Pas de tir 14">Pas de tir 14</option>
-            <option value="Pas de tir 15">Pas de tir 15</option>
-            <option value="Pas de tir 16">Pas de tir 16</option>
-            <option value="Pas de tir 17">Pas de tir 17</option>
-            <option value="Pas de tir 18">Pas de tir 18</option>
-            <option value="Pas de tir 19">Pas de tir 19</option>
-            <option value="Pas de tir 20">Pas de tir 20</option>
-        </select>  
+            <select class="form-control w-25 mt-2" id="horaire">
+                <option value="08:00">08:00 - 08:30</option>
+                <option value="08:30">08:30 - 09:00</option>
+                <option value="09:00">09:00 - 09:30</option>
+                <option value="09:30">09:30 - 10:00</option>
+                <option value="10:00">10:00 - 10:30</option>
+                <option value="10:30">10:30 - 10:00</option>
+                <option value="11:00">11:00 - 11:30</option>
+                <option value="11:30">11:30 - 11:00</option>
+                <option value="12:00">12:00 - 12:30</option>
+                <option value="12:30">12:30 - 12:00</option>
+                <option value="13:00">13:00 - 13:30</option>
+                <option value="13:30">13:30 - 13:00</option>
+                <option value="14:00">14:00 - 14:30</option>
+                <option value="14:30">14:30 - 14:00</option>
+                <option value="15:00">15:00 - 15:30</option>
+                <option value="15:30">15:30 - 15:00</option>
+                <option value="16:00">16:00 - 16:30</option>
+                <option value="16:30">16:30 - 17:00</option>
+                <option value="17:00">17:00 - 17:30</option>
+                <option value="17:30">17:30 - 18:00</option>
+                <option value="18:00">18:00 - 18:30</option>
+                <option value="18:30">18:30 - 19:00</option>
+                <option value="19:00">19:00 - 19:30</option>
+                <option value="19:30">19:30 - 20:00</option>
+                <option value="20:00">20:00 - 20:30</option>
+                <option value="20:30">20:30 - 21:00</option>
+            </select> 
         </div>
+
         <div class="mt-2">
-        <button type="submit" class="btn btn-primary">Réserver</button>
+            <select class="form-control w-25" id="pastir">
+                <option value="1">Pas de tir 1</option>
+                <option value="2">Pas de tir 2</option>
+                <option value="3">Pas de tir 3</option>
+                <option value="4">Pas de tir 4</option>
+                <option value="5">Pas de tir 5</option>
+                <option value="6">Pas de tir 6</option>
+                <option value="7">Pas de tir 7</option>
+                <option value="8">Pas de tir 8</option>
+                <option value="9">Pas de tir 9</option>
+                <option value="10">Pas de tir 10</option>
+                <option value="11">Pas de tir 11</option>
+                <option value="12">Pas de tir 12</option>
+                <option value="13">Pas de tir 13</option>
+                <option value="14">Pas de tir 14</option>
+                <option value="15">Pas de tir 15</option>
+                <option value="16">Pas de tir 16</option>
+                <option value="17">Pas de tir 17</option>
+                <option value="18">Pas de tir 18</option>
+                <option value="19">Pas de tir 19</option>
+                <option value="20">Pas de tir 20</option>
+            </select>  
+        </div>
+
+        <div class="mt-2">
+            <button type="submit" class="btn btn-primary">Réserver</button>
         </div>
     </form>
     
