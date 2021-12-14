17327be7891d24222bdeeb76c6861f2d4189a589
Kevin <kvanconingsloo@gmail.com>
2021-12-14 19:25:06 +0100

    check params activeUser

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -1,5 +1,7 @@
 <?php
 
+use function PHPUnit\Framework\isEmpty;
+
 /**
  * Controller admin
  */
@@ -20,45 +22,57 @@ class Admincontroller
             $myView = new View("confirmUserPage");
             $myView->render($users);
         }
+        $m = null;
     }
 
     public function activateUser()
     {
-        $id = intval($_POST['idUser']);
-
         $m = new Manager();
 
-        if($m->activateUserDb($id)) {
-
-
-            $user = $m->getUserById($id);
-
-            $mdp = $m->encrypt_decrypt($user->getPassword(), "decrypt");
-
-            $email_msg = "Votre compte est bien activé ! \n\n";
-            $email_msg .= "Nom : " . $user->getName() . "\n";
-            $email_msg .= "Prénom : " . $user->getFirstname() . "\n";
-            $email_msg .= "Email: " . $user->getMail() . "\n";
-            $email_msg .= "Password: " . $mdp . "\n";
-
-
-            $dest = $user->getMail();
-            $sujet = "Confirmation de compte";
-
-            $headers = 'From: TKT@hotmail.com' . "\r\n" .
-                'Reply-To: '. $user->getMail(). ' "\r\n"' .
-                'X-Mailer: PHP/' . phpversion();
-
-            mb_send_mail($dest, $sujet, $email_msg, $headers);   
-               
+        if (isEmpty($_POST['idUser'])){
             $users = $m->getAllUsers();
 
             $myView = new View("confirmUserPage");
             $myView->render($users);
-        } else {
+        }else{
+            $id = intval($_POST['idUser']);
 
-            var_dump("probleme");die();
+
+            if ($m->activateUserDb($id)) {
+
+
+                $user = $m->getUserById($id);
+
+                $mdp = $m->encrypt_decrypt($user->getPassword(), "decrypt");
+
+                $email_msg = "Votre compte est bien activé ! \n\n";
+                $email_msg .= "Nom : " . $user->getName() . "\n";
+                $email_msg .= "Prénom : " . $user->getFirstname() . "\n";
+                $email_msg .= "Email: " . $user->getMail() . "\n";
+                $email_msg .= "Password: " . $mdp . "\n";
+
+
+                $dest = $user->getMail();
+                $sujet = "Confirmation de compte";
+
+                $headers = 'From: TKT@hotmail.com' . "\r\n" .
+                'Reply-To: ' . $user->getMail() . ' "\r\n"' .
+                    'X-Mailer: PHP/' . phpversion();
+
+                mb_send_mail($dest, $sujet, $email_msg, $headers);
+
+                $users = $m->getAllUsers();
+
+                $myView = new View("confirmUserPage");
+                $myView->render($users);
+            } else {
+
+                var_dump("probleme");
+                die();
+            }
         }
+        $m = null;
+        
     }
 }
 ?>
