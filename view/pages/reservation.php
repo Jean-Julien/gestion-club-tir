1c669af74b6363cca10cfc9fce64ed3b1f985f31
R3V3NG3 <petitpiqueur@gmail.com>
2022-01-17 19:19:18 +0100

    User story 15

--- view/pages/reservation.php
+++ view/pages/reservation.php
@@ -1,40 +1,3 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="<?php echo IMG ?>logo.png" alt="logo" width="32" height="32">
-            Projet TKT
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?r=home">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php?r=reservation">Réservation</a>
-                </li>
-            </ul>
-            <div class="flex-shrink-0 dropdown">
-                <a href="#" class="d-block link-light text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                    <?php echo "Bienvenue " . $_SESSION['prenom']; ?>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-lg-end dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser2">
-                    <li><a class="dropdown-item" href="#">Mon profil</a></li>
-                    <?php if ($_SESSION['idRole'] == '1') { ?>
-                        <li><a class="dropdown-item" href="index.php?r=admin/confirmuser">Confirmation users</a></li>
-                    <?php } ?>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="index.php?r=logout">Se déconnecter</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</nav>
-
 <div class="container p-4 mt-3" style="margin-bottom: 60px;">
     <form class="shadow py-4 px-3 bg-dark bg-gradient rounded row g-3" role="form" action="<?php echo insertReservation ?>" method="post">
         <div class="mb-1">
@@ -50,11 +13,6 @@
             echo "<div class='alert alert-success' role='alert'>" . $_SESSION['reserv_success'] . "</div>";
         }
         ?>
-
-        <!--<div class="col-12">
-            <label for="pseudo" class="form-label text-white-50">Pseudo</label>
-            <input type="text" class="form-control" id="pseudo" name="reserv_pseudo" placeholder="Insérez votre Pseudo">
-        </div>-->
 
         <input type="hidden" name="user_id" value="<?php echo $_SESSION['id'] ?>">
 
@@ -146,7 +104,6 @@
                 <?php foreach ($params as $pasdetir) : ?>
                     <option value="<?php echo $pasdetir->idPasDeTir; ?>"><?php echo $pasdetir->nomPasDeTir; ?></option>
                 <?php endforeach; ?>
-                <option value="1">iuiui</option>
             </select>
         </div>
 
